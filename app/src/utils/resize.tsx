c10c400e0fa27795bf8d1ec9c252144a0fc518ea
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-08-14 03:21:46 +0530

    feat: create resizing utilities

---
+++ app/src/utils/resize.tsx
@@ -0,0 +1,27 @@
+export const resize = (width: number, height: number, ratio: number = 16/9): { width: number, height: number } => {
+    const currentRatio = width / height;
+
+    if (currentRatio > ratio) {
+        return { 
+            width: Math.round(height * ratio),
+            height
+        }
+    } else if (currentRatio < ratio) {
+        return {
+            width,
+            height: Math.round(width / ratio)
+        }
+    }
+
+    return { width, height }
+}
+
+export const calculateSizeReductionScale = (width: number, height: number): number => {
+	if (width <= 600) {
+		return 1.2;
+	} else if (width <= 800) {
+		return 1.5;
+	}
+
+	return 2;
+};
