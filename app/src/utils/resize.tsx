35fc25f6b23b97ff5613c99fde7de4b7f6945211
AndreiBalan-dev <jenialulegion0@gmail.com>
2024-08-16 02:48:58 +0300

    video player & components major update

--- app/src/utils/resize.tsx
+++ app/src/utils/resize.tsx
@@ -1,27 +1,31 @@
-export const resize = (width: number, height: number, ratio: number = 16/9): { width: number, height: number } => {
-    const currentRatio = width / height;
+export const resize = (
+  width: number,
+  height: number,
+  ratio: number = 16 / 9
+): { width: number; height: number } => {
+  const currentRatio = width / height;
 
-    if (currentRatio > ratio) {
-        return { 
-            width: Math.round(height * ratio),
-            height
-        }
-    } else if (currentRatio < ratio) {
-        return {
-            width,
-            height: Math.round(width / ratio)
-        }
-    }
+  if (currentRatio > ratio) {
+    return {
+      width: Math.round(height * ratio),
+      height,
+    };
+  } else if (currentRatio < ratio) {
+    return {
+      width,
+      height: Math.round(width / ratio),
+    };
+  }
 
-    return { width, height }
-}
+  return { width, height };
+};
 
 export const calculateSizeReductionScale = (width: number): number => {
-	if (width <= 600) {
-		return 1.2;
-	} else if (width <= 800) {
-		return 1.5;
-	}
+  if (width <= 600) {
+    return 1.2;
+  } else if (width <= 800) {
+    return 1.5;
+  }
 
-	return 2;
+  return 2;
 };
